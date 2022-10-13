ca2f22b5fd77db1957455fa23013fde0481f354f
GTsatiris <george.tsatiris@gmail.com>
2022-10-13 19:02:13 +0300

    Basic OpenGL App finished

--- VisualStudio/1_Introduction_to_OpenGL/1_Introduction_to_OpenGL/1_Introduction_to_OpenGL.cpp
+++ VisualStudio/1_Introduction_to_OpenGL/1_Introduction_to_OpenGL/1_Introduction_to_OpenGL.cpp
@@ -5,6 +5,10 @@
 
 using namespace std;
 
+//Callbacks and other function
+void framebufferSizeCallback(GLFWwindow* window, int width, int height);
+void processKeyboardInput(GLFWwindow* window);
+
 //Settings
 unsigned int SCREEN_WIDTH = 1280;
 unsigned int SCREEN_HEIGHT = 1024;
@@ -30,6 +34,76 @@ int main()
     glfwMakeContextCurrent(window);
     
     //Register callbacks
-    
+    glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
     //--------------------
+
+    //Load OpenGL function pointers using GLAD
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        cout << "Failed to initialise GLAD and load OpenGL function pointers!!!" << endl;
+        glfwTerminate();
+        return -1;
+    }
+    //----------------------------------------
+
+    //Shader loading and compiling
+    // TODO
+    //----------------------------
+
+    //Geometry loading
+    // TODO
+    //----------------
+
+    //Texture loading
+    // TODO
+    //---------------
+
+    //MAIN RENDERING LOOP
+    while (!glfwWindowShouldClose(window))
+    {
+        //Handling keyboard input
+        processKeyboardInput(window);
+        //-----------------------
+
+        //Initialising buffers
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        //--------------------
+
+        //Geometry rendering
+        // TODO
+        //------------------
+
+        //Buffer swapping
+        glfwSwapBuffers(window);
+        //---------------
+
+        //Polling for events (key pressed/released, mouse, etc.)
+        glfwPollEvents();
+        //------------------
+    }
+    //-------------------
+
+    //Releasing structures and memory
+    // TODO release allocated buffers
+    glfwTerminate();
+    //-------------------------------
+
+    return 0;
+}
+
+void processKeyboardInput(GLFWwindow* window)
+{
+    //If ESC is pressed, close the window
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    {
+        glfwSetWindowShouldClose(window, true);
+    }
+}
+
+void framebufferSizeCallback(GLFWwindow* window, int width, int height)
+{
+    //Update OpenGL viewport (i.e. the frame buffer) to match the dimensions of the GLFW window
+    //after resizing.
+    glViewport(0, 0, width, height);
 }
