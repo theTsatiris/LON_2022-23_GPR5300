aed0c4a9039f60f77b56132b6ce367e4b1eb4a7c
GTsatiris <george.tsatiris@gmail.com>
2022-10-13 17:46:37 +0300

    started adding opengl code

--- VisualStudio/1_Introduction_to_OpenGL/1_Introduction_to_OpenGL/1_Introduction_to_OpenGL.cpp
+++ VisualStudio/1_Introduction_to_OpenGL/1_Introduction_to_OpenGL/1_Introduction_to_OpenGL.cpp
@@ -1,20 +1,35 @@
-// 1_Introduction_to_OpenGL.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 
 #include <iostream>
 
+using namespace std;
+
+//Settings
+unsigned int SCREEN_WIDTH = 1280;
+unsigned int SCREEN_HEIGHT = 1024;
+//--------
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    //Initialise and configure GLFW
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    //-----------------------------
+
+    //GLFW Window creation
+    GLFWwindow* window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "GPR5300 2022 - Intro to OpenGL", NULL, NULL);
+    if (window == NULL)
+    {
+        cout << "Failed to create GLFW window!!!" << endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    
+    //Register callbacks
+    
+    //--------------------
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
